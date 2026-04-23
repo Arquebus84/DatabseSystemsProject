ba680aab577015a4e187fc86924256e546314f91
WackyTubeman <99372113+WackyTubeman@users.noreply.github.com>
2026-04-23 03:39:20 -0400

    Updated medication and family controller to prevent potential race condition

--- src/controllers/familyController.js
+++ src/controllers/familyController.js
@@ -4,14 +4,14 @@ exports.getFamilyTable = (req, res) => {
     // SQL command. Names mush match name in index.js
     const sql = `
         SELECT tf.familyLastName AS familyName, pn.phoneNumber AS phoneNumber
-        FROM trusted_family tf 
-        JOIN phone_number pn
+        FROM trusted_family tf
+                 JOIN phone_number pn
         WHERE tf.phoneNumberID = pn.numberID
     `;
 
     // Query server with SQL command
     pool.query(sql, (err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
+        if (err) return res.status(500).json({error: err.message});
         res.json(results);
     });
 };
@@ -20,35 +20,34 @@ exports.getFamilies = (req, res) => {
     // SQL command. Names mush match name in index.js
     const sql = `
         SELECT tf.familyLastName AS familyName, tf.familyID AS familyID
-        FROM trusted_family tf 
+        FROM trusted_family tf
     `;
 
     // Query server with SQL command
     pool.query(sql, (err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
+        if (err) return res.status(500).json({error: err.message});
         res.json(results);
     });
 }
 
 exports.setFamilyTable = (req, res) => {
-    const { familyName, phoneNumber } = req.body;
+    const {familyName, phoneNumber} = req.body;
 
     // Run the insert
-    const phoneNumSql = `INSERT INTO phone_number (phoneNumber) VALUES (?)`;
+    const phoneNumSql = `INSERT INTO phone_number (phoneNumber)
+                         VALUES (?)`;
 
     pool.query(phoneNumSql, [phoneNumber], (err, result) => {
         if (err) return res.status(500).json({error: err.message});
 
-        pool.query("SELECT MAX(numberID) AS maxID FROM phone_number", (err, results) => {
-            const phoneID = (results[0].maxID || 0);
+        const newPhoneID = result.insertId;
+        // Run the insert
+        const familySql = `INSERT INTO trusted_family (familyLastName, phoneNumberID)
+                           VALUES (?, ?)`;
 
-            // Run the insert
-            const familySql = `INSERT INTO trusted_family (familyLastName, phoneNumberID) VALUES (?, ?)`;
-
-            pool.query(familySql, [familyName, phoneID], (err, result) => {
-                if (err) return res.status(500).json({error: err.message});
-                res.json({message: "Saved successfully"});
-            });
+        pool.query(familySql, [familyName, newPhoneID], (err, result) => {
+            if (err) return res.status(500).json({error: err.message});
+            res.json({message: "Saved successfully"});
         });
     });
 }
