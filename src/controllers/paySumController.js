3ad16d809d0cdb2122faa90a8d472a53c7d73f0e
Arquebus84 <matteoventura06@gmail.com>
2026-05-01 12:58:03 -0400

    Modified sql query in paySumController.js for consistency

--- src/controllers/paySumController.js
+++ src/controllers/paySumController.js
@@ -1,13 +1,6 @@
 const pool = require('../models/database');
 
 exports.getPaySumTable = (req,res)=>{
-    // const sql = `SELECT ROUND(sum(sys.price * sys.tax * 0.01 + sys.price), 2) AS netPayment, p.lastName AS patientName FROM payment_summary psum 
-    //     JOIN patient p
-    //     JOIN payment_system sys
-    //     WHERE p.patientID = psum.patientID AND psum.paymentID = sys.paymentID
-    //     GROUP BY p.lastName;
-    // `;
-
     const sql = `SELECT ROUND(sum(sys.price * sys.tax * 0.01 + sys.price), 2) AS netPayment, p.lastName AS patientName
         FROM payment_system sys 
         JOIN patient p 
